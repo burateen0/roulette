566b2f42d1e10436c7d3ded7b1138a900312ecd4
burateen0 <73470516+burateen0@users.noreply.github.com>
2025-10-17 20:18:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 let money = 100;
 
 function calcColumn(column) {
@@ -7,9 +6,7 @@ function calcColumn(column) {
   for (let i = 0; i < 25; i++) {
     const random = Math.floor(Math.random() * 10);
     const cellColor = (i % 2) ? 'middle' : '';
-    columnElement.innerHTML += `
-      <li class="${cellColor}">${random}</li>
-    `;
+    columnElement.innerHTML += `<li class="${cellColor}">${random}</li>`;
   }
 }
 
@@ -28,20 +25,13 @@ function resetAnimation(column) {
   ulElement.style.transition = 'none';
   ulElement.style.left = '0';
   setTimeout(() => {
-    const randomDuration = Math.random() * 6 + 3; 
-    ulElement.style.transition = `${randomDuration}s ease`; 
+    ulElement.style.transition = `0.7s ease`;  
   }, 10);
 }
 
-let isStarted = false;
-
 function start() {
-  if (isStarted) return;
-  isStarted = true;
-
   if (money < 10) {
     alert("Недостаточно средств чтобы играть!");
-    isStarted = false;
     return;
   }
 
@@ -50,7 +40,7 @@ function start() {
 
   const columns = ['col1', 'col2', 'col3'];
   const isJackpot = Math.random() < 0.11;
-  let jackpotNumber = Math.floor(Math.random() * 10);
+  const jackpotNumber = Math.floor(Math.random() * 10);
 
   columns.forEach((column) => {
     calcColumn(column);
@@ -71,7 +61,6 @@ function start() {
     const index = -Math.floor((move + (elm('.scopeHidden').offsetWidth / 2) / -150) / 150) + 1;
 
     if (isJackpot) {
-      // Вставляем одинаковое число в центр
       columns.forEach((column) => {
         elms(`.${column} > li`)[index].textContent = jackpotNumber;
       });
@@ -79,28 +68,14 @@ function start() {
 
     const results = columns.map((column) => parseInt(elms(`.${column} > li`)[index].textContent));
 
-    setTimeout(() => {
-      if (results[0] === results[1] && results[1] === results[2]) {
-        setTimeout(() => {
-          money += 10000;
-          alert("🎉 Джекпот! Вы выигрываете 10 000 монет!");
-          updateMoneyDisplay();
-          isStarted = false;
-        }, 100);
-      } else {
-        updateMoneyDisplay();
-        isStarted = false;
-      }
-      console.log(results);
-    }, 999); 
-  }, 600); 
+    if (results[0] === results[1] && results[1] === results[2]) {
+      money += 10000;
+      alert("🎉 Джекпот! Вы выигрываете 10 000 монет!");
+    }
+
+    updateMoneyDisplay();
+    console.log(results);
+  }, 700); 
 }
-
-
-
-  // columns.forEach((column, i) => {
-  //   elms(`.${column} > li`)[index].style.background = 'red';
-  // });
-
 
 window.onload = initialize;
